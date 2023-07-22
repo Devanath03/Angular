625ff4fa29aafa036d4510875daada155ea16d1d
Devanath03 <devanathbalann@gmail.com>
2023-07-22 14:45:33 +0530

    first commit

---
+++ File-Upload/src/app/dumpy/dumpy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dumpy',
+  templateUrl: './dumpy.component.html',
+  styleUrls: ['./dumpy.component.css']
+})
+export class DumpyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
